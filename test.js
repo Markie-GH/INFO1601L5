81a8bf4784ee4edf29feb278942c6c9ec7154a01
Markie-GH <jermarkie@gmail.com>
2025-03-03 20:07:25 -0400

    tested again for understanding

---
+++ test.js
@@ -0,0 +1,35 @@
+let person = {
+    // key : value
+    "name": "John",
+    "age": 12
+  };
+  
+  //we can print the values of a key in two ways
+  console.log(person["age"]);//Using an index
+  console.log(person.name);//referencing the key as a property
+  
+  person["weight"] = 70;
+  
+  person.marks = [67, 34, 55, 89];
+  
+  let firstMark = person.marks[0];
+  
+  //mixing array and objects
+  let people = [
+    person,
+    {
+      name: "Jane",
+      age: 23,
+      marks: [51, 78, 99, 76]
+    }
+  ];
+  
+  let johnClone = {};//empty object
+  
+  //copying objects
+  Object.assign(johnClone, person);
+  
+  console.log(johnClone);
+  
+  
+  console.log(people[0].marks[0]);//what is printed?
